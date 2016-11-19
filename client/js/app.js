8a1f5510878efc77b4304ac22779650b35f13d66
Gavin Haynes <haynes.gt@gmail.com>
2016-11-19 14:42:37 -0800

    working on bug and on client

--- client/js/app.js
+++ client/js/app.js
@@ -1,11 +1,41 @@
-angular.module('appName', [])
-.directive('antGame', function() {
+var resource;
+
+angular.module('appName', ['ngResource'])
+.controller('antGameController', function($scope, $http, $resource) {
+  resource = $resource;
+  var users = $resource('/api/users');
+  var colonies = $resource('/api/colonies');
+
+  var gameReload = function() {
+    colonies.get({}, function(data) {
+      $scope.colonies = data;
+    });
+  };
+
+  gameReload();
+
+  $scope.begin = function() {
+    user = users.save({}, function(userData) {
+      console.log(userData);
+      colony = colonies.save(
+        {},
+        {userId: userData.id, colonyName: $scope.beginData.colonyName},
+        function(colonyData) {
+          console.log(colonyData);
+          gameReload();
+          $scope.hasColony = true;
+      });
+    });
+  };
+
+  $scope.beginData = {};
+  $scope.hasColony = false;
+
+})
+.directive('antGame', function($http) {
   return {
     restrict: 'AE',
-    templateUrl: '/html/antGame.html',
-    link: function(scope, elem, attrs) {
-      scope.colonies = [{name: 'first'}, {name: 'second'}];
-    }
+    templateUrl: '/html/antGame.html'
   };
 });
 
