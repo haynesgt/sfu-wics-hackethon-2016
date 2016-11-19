e723e023b2c6ea6033f74c5bdbd0e51c385d0255
Gavin Haynes <haynes.gt@gmail.com>
2016-11-19 10:05:11 -0800

    Install server

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var JsonDB = require('node-json-db');
-var db = new JsonDB('database/ants',
+var db = new JsonDB(
+  'database/ants',
   true, // save after each push
   false // not human readable
 );
 
-db.push('/colonies/1', 'colony1');
+db.push('/colonies', {name: 'me'});
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res, err) {
+  res.end(JSON.stringify(db.getData('/colonies')));
+});
+
+app.listen(8080, function() {
+  console.log('app listening on port 8080...');
+});
